79319981502c817cb8d7cca8cd8c0e06d9a62b7a
kendall <kendall.poindexter@detroitlabs.com>
2020-05-25 11:18:17 -0400

    Create custom cell class and file organization

---
+++ weatherAppAlpha/weatherAppAlpha/Views/DailyForcastTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DailyForcastTableViewCell.swift
+//  weatherAppAlpha
+//
+//  Created by Kendall Poindexter on 5/24/20.
+//  Copyright © 2020 Kendall Poindexter. All rights reserved.
+//
+
+import UIKit
+
+class DailyForcastTableViewCell: UITableViewCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var lowTemperatureLabel: UILabel!
+    @IBOutlet weak var highTemperatureLabel: UILabel!
+    
+
+}
