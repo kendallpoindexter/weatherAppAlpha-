071b7cbf807f3b82f8af492df4214bd093f2edd0
kendall <kendall.poindexter@detroitlabs.com>
2020-05-25 16:31:05 -0400

    Create MainViewModel and leverage it in the the MainViewController

--- weatherAppAlpha/weatherAppAlpha/Views/MainViewController.swift
+++ weatherAppAlpha/weatherAppAlpha/Views/MainViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import CoreLocation
 
 class MainViewController: UIViewController {
     @IBOutlet weak var dailyForcastTableView: UITableView!
     
+    let viewModel = MainViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        viewModel.locationManager.delegate = self
+        viewModel.requestUserLocation()
     }
 }
 
+extension MainViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        viewModel.lat = location?.coordinate.latitude
+        viewModel.lon = location?.coordinate.longitude
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        //Alert pop up
+        print(error)
+    }
+}
